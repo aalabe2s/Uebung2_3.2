0304022e195d653b68e473957d6da96f9addf43e
Abdulrahman Alabed <aabde.alabed1998@gmail.com>
2023-04-16 09:28:25 +0200

    Aufgab_3.3 Test datei erstelle, ist aber noch nicht fertig

---
+++ aufgabe_3_3/src/main/java/org/example/Sortieren.java
@@ -0,0 +1,11 @@
+package org.example;
+public class Sortieren implements SortierenIF{
+    public static void main(String[] args) {
+        
+    }
+
+    @Override
+    public String[] isWellSorted(String[] sequence) {
+        return null;
+    }
+}
---
+++ aufgabe_3_3/src/test/java/org/example/SortierenTest.java
@@ -0,0 +1,34 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SortierenTest {
+
+    String [][] sequence = {{"A", "B"},
+                            {"C", "D"},
+                            {"E", "F"}};
+    String [] array= {"A", "B", "D", "C"};
+    Sortieren s = new Sortieren();
+    @Test
+    void test(){
+        String [] s2 = s.isWellSorted(array);
+
+        while (i<array.length){
+            for(j = 0; j<sequence[0].length; j++){
+                for(k=0; k<sequence.length)
+            }
+            if (array[i] == sequence)
+        }
+
+        for (i = 0; i < array.length; i++){
+            for (j = 0; j<sequence.length; j++){
+
+            }
+        }
+
+
+    }
+
+}
